4bb246f1d3113b68183f56204f03b3b6b861c868
Timo Tuominen <tituomin@gmail.com>
2014-03-15 15:33:52 +0100

    Add js files, include them in the template.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,16 @@
 
+
+var hash = window.location.hash.substring(1);
+selectNeighborhood(hash);
+
+function handleNeighborhood(obj, status) {
+    console.log(obj);
+    $('h1').text(obj.name + ' 2050');
+}
+
+
 function selectNeighborhood(nid) {
-    
+    $.ajax('http://dev.hel.fi/geocoder/v1/district/' + nid, {'success': handleNeighborhood})
 }
 
 function getRespondents(nid) {
@@ -30,3 +40,4 @@ function sortRespondentValues(attribute, nid) {
     var sorted = _.sortBy(vals, 'count');
     return sorted;
 }
+
