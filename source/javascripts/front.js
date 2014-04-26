9c8fb4d78dedf0826f4e3112ae6c27f895387629
terotic <tero.tikkanen@gmail.com>
2014-04-27 00:31:06 +0300

    new version

---
+++ source/javascripts/front.js
@@ -0,0 +1,15 @@
+
+var neighborhoods = {}
+
+function handlePoints(obj, status) {
+    _.each(obj['objects'], function(o) {
+        if (o.type === 'kaupunginosa') {
+            neighborhoods[o.origin_id] = o;
+        }}
+    );
+    var sorted = _.sortBy(_.values(neighborhoods), 'name');
+    _.each(sorted, function(o) {$('#neighborhoods').append('<div class="neighborhood"><a href="/kaupunginosa#' + o.id  +'">' + o.name + '</a></div>')})
+    
+}
+
+$.ajax('http://dev.hel.fi/geocoder/v1/district/?limit=300', {'success': handlePoints})
